c359810a148292aefe9981356dc2aa1a4d2ecb33
Tayná <tayna.stoski@escola.pr.gov.br>
2026-03-20 15:00:12 +0000

    true-false

--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -1,4 +1,10 @@
 //booleano
 
-const usuariologado = true;
-const contapaga = false;
+const usuarioLogado = true;
+const contaPaga = false;
+
+//falso e verdadeiro
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
