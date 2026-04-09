20c54193d45618eb28a802fe17a0023a525932a6
Tayná <tayna.stoski@escola.pr.gov.br>
2026-04-09 15:19:47 +0000

    calculadora inteligente

--- conversoes.js
+++ conversoes.js
@@ -1,12 +1,21 @@
 //tipo de dados
 //booleanos
 
-//conversao implícita
-const numero = 456;
-const numeroString = Number("456");
+// //conversao implícita
+//const numero = 456;
+//const numeroString = Number("456");
 
 //Number()
 //String()
-console.log(numero + numeroString);
+//console.log(numero + numeroString);
 
-//conversão explícitas
+// //conversão explícitas
+
+//calculadora inteligente
+
+const preco = "100.50";
+const desconto = "10";
+
+console.log(preco - desconto);
+console.log(preco * 2);
+console.log(preco / 2);
