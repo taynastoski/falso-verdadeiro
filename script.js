0ad2b530fb71798fb5c99887c990fa8811662c24
Tayná <tayna.stoski@escola.pr.gov.br>
2026-04-30 15:08:23 +0000

    .

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let dado = 100;
 console.log("Valor", dado);
 console.log("Tipo", typeof dado);
 
-//
+//Mudando o tipo da mesma variavel (o que define a tipagem dinamica)
+dado ="Olá, sou um texto";
+console.log("Novo valor", dado);
+console.log("Novo tipo", typeof dado);
