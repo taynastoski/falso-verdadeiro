060b3db50008839d88c98753995e3c70573dcd1e
Tayná <tayna.stoski@escola.pr.gov.br>
2026-04-30 15:21:57 +0000

    .

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //EXEMPLO DE TIPAGEM DINAMICA
-let dado = 100;
-console.log("Valor", dado);
-console.log("Tipo", typeof dado);
+//let dado = 100;
+//console.log("Valor", dado);
+//console.log("Tipo", typeof dado);
 
 //Mudando o tipo da mesma variavel (o que define a tipagem dinamica)
-dado ="Olá, sou um texto";
-console.log("Novo valor", dado);
-console.log("Novo tipo", typeof dado);
+//dado = "Olá, sou um texto";
+//console.log("Novo valor", dado);
+//console.log("Novo tipo", typeof dado);
+
+//Exemplo da diferença entre let e const
+const cpf = "123.456.789-00";
+console.log("cpf", cpf);
+
+
