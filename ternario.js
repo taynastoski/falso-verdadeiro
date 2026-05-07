517edc56d2ffd9fa10abeaeffe89e449f1d96b91
Tayná <tayna.stoski@escola.pr.gov.br>
2026-05-07 15:17:35 +0000

    ctf

--- ternario.js
+++ ternario.js
@@ -1,8 +1,13 @@
 // ternario: uso de if e else
 
-const idadeMinima = 18;
-const idadeCliente = 16;
+//const idadeMinima = 18;
+//const idadeCliente = 20;
 
-if (idadeCliente >= idadeMinima){
-    
-}
+//if (idadeCliente >= idadeMinima){
+//    console.log("cerveja");
+//} else {
+//    console.log("suco");
+//}
+
+     // condição        //true      //false
+console.log(idadeCliente >= idadeMinima ? "cerveja": "suco");
