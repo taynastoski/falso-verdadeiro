f6fad729c0fd38b84d24cdb1e60fc41594df87a1
Tayná <tayna.stoski@escola.pr.gov.br>
2026-05-21 15:06:13 +0000

    .

---
+++ funcoes.js
@@ -0,0 +1,29 @@
+// Funções
+
+console.log("--- INICIO DO TESTE DA TURMA ---");
+console.log(""); 
+
+//EXEMPLO 1: Função que executa uma ação direta
+
+//1) Declarando a função (criando a máquina)
+function mostrarMensagem(texto) {
+    console.log("Exemplo 1 -> Mensagem recebida: " + texto);
+}
+
+//2) Executando a função (ligando a máquina)
+mostrarMensagem("Bem-vindos ao GitHub Codespace!");
+mostrarMensagem("Testando a minha primeira função!");
+
+console.log("");
+
+//EXEMPLO 2: Função que calcula e DEVOLVE um valor (return)
+
+//1)Declarando a função que soma dois números
+function calcularSoma("num1, num2") {
+    return num1 + num2; 
+}
+
+//2) Executando e mstrando o resultado direto no console
+console.log("Exemplo 2 -> O resultado da soma de 5 + 3 é: " calcularSoma(5, 3));
+
+//3)
